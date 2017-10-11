8ec031a6b5190abed9867b9a38488978a8a65755
Edmond Atto <edmond091@gmail.com>
2017-10-11 15:21:50 +0300

    Add ml_variables practice file

--- ml_variables.py
+++ ml_variables.py
@@ -1,17 +1,40 @@
 import tensorflow as tf
 
-w = tf.Variable([2.4, 5.0], tf.float32, name='w')
-x = tf.placeholder(tf.float32, name='x')
-b = tf.Variable([9.0, 7.4], tf.float32, name='b')
+# y = Wx + b
 
-y = w * x + b
+W = tf.Variable([2.0, 3.5], tf.float32, name='var_W')
+x = tf.placeholder(tf.float32, name='x')
+b = tf.Variable([1, 4.4], tf.float32, name='var_b')
+
+y = W * x + b
 
 init = tf.global_variables_initializer()
 
 with tf.Session() as sess:
     sess.run(init)
-    print(sess.run(y, feed_dict={x: [3, 5]}))
 
-writer = tf.summary.FileWriter('./m2_ex_6', sess.graph)
-writer.close()
+    result = sess.run(y, feed_dict={x: [10, 100]})
+    print('Final Result Is: {}'.format(result))
+
+# Calculating intermediate values
+s = W * x
+init = tf.variables_initializer([W])
+
+with tf.Session() as sess:
+    sess.run(init)
+
+    result = sess.run(s, feed_dict={x: [10, 100]})
+    print('Final Result: {}'.format(result))
+
+number = tf.Variable(2)
+multiplier = tf.Variable(6)
+
+init = tf.global_variables_initializer()
+result = number.assign(tf.multiply(number, multiplier))
+
+with tf.Session() as sess:
+    sess.run(init)
+    for i in range(10):
+        print(sess.run(result))
+        print('New Number Value: {}\n'.format(sess.run(number.assign_add(1))))
 
